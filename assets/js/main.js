f58e79a16054f2605d55dfb2d2686072e81d0451
DESKTOP-NTVJAD1\5070 <hijaab.sikander@appsnation.co>
2024-06-26 18:12:39 +0500

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,27 +23,28 @@ $(document).ready(function() {
     })
 });
 
-$(document).ready(function() {
-    $('.bl-carousel').owlCarousel({
-        items:3,
-        loop:true,
-        margin:10,
-        nav:false,
-        dots:true,
-        autoplay:true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:true,
-        slideBy: 4,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:4
-            }
+
+$(document).ready(function(){
+    $(".blog-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 2
+        },
+        1000: {
+          items: 3
         }
-    })
-});
+      }
+    });
+  });
+
+
+
+
+
+
